28b4171fb6a05aab5644e21b036cad3af16d5e60
bl03360 <54645762+bl03360@users.noreply.github.com>
2020-04-21 21:25:20 -0400

    Add files via upload
    Finished file reader method 
    Started on outer and inner hashmap for the dfa
    Need to work on actually running strings through it to test if it is a proper language of the dfa

--- DFA.java
+++ DFA.java
@@ -6,23 +6,28 @@ import java.util.*;
 
 public class DFA {
 
-	HashMap<Character, HashMap> dfa = new HashMap<>();
+	private static HashMap<Character, Character> inner;
+	private static HashMap<Character, HashMap<Character, Character>> outer = new HashMap<>();
+	private static ArrayList<Character> alph = new ArrayList<>();
+	private static ArrayList<Character> states = new ArrayList<>();
+	private static ArrayList<Character> finalStates = new ArrayList<>();
+	private static LinkedList<Character> transitions = new LinkedList<>();
+	private static char startState;
+	
 	
 	public static void main(String[] args) {
-
-		ArrayList<Character> alph = new ArrayList<>();
-		ArrayList<Character> states = new ArrayList<>();
-		ArrayList<Character> finalState = new ArrayList<>();
-		char startState = 0;	
+	
 		char marker = 0;
 		
-		fileReader(alph, states, finalState, startState);
+		fileReader();
+		createDfa();
+		
+		Scanner scan = new Scanner(System.in);
 		
 
 	}
 
-	private static void fileReader(ArrayList<Character> alph, ArrayList<Character> states, ArrayList<Character> finalState, char startState) {
-		
+	private static void fileReader() {
 		try {
 			File file = new File("");
 			FileReader fr = new FileReader(file);
@@ -33,46 +38,66 @@ public class DFA {
 			
 			while (line != null) {
 				
+				line.replaceAll("[^a-zA-Z0-9]+", "");
 				StringBuilder sb = new StringBuilder();
 				sb.append(line);
 				
 				while(count != 3) {
 					if(count == 0) {
 						for(int i = 0;i<sb.length();i++) {
-							if(sb.charAt(i) != '{' || sb.charAt(i) != ',' || sb.charAt(i) != '}' ) {
-								alph.add(sb.charAt(i));
-							}
+							alph.add(sb.charAt(i));
 						}
 					}//reads alphabet from line 1 from file
 					else if(count == 1) {
 						for(int i = 0;i<sb.length();i++) {
-							if(sb.charAt(i) != '{' || sb.charAt(i) != ',' || sb.charAt(i) != '}' ) {
-								states.add(sb.charAt(i));
-							}
+							states.add(sb.charAt(i));
 						}
 					}//reads states from line 2 from file
 					else if(count ==  2){
 						startState = sb.charAt(0);
 					}//reads start state from line 3 from file
 					else {
-						
+						for(int i = 0;i<sb.length();i++) {
+							finalStates.add(sb.charAt(i));
+						}
 					}//reads final states
 					sb.append("");
 					line = br.readLine();
 					count++;
 				}//reads first 4 lines for the alphabet, states, start state, and final states
 				
+				for(int i = 0;i<sb.length();i++) {
+						transitions.add(sb.charAt(i));
+				}//reads transitions from file adding from rest of the lines
 				
-			}
+			}//while loop to read all lines of file
 		
-		}
+		}//try block
 		catch(IOException error) {
 			error.printStackTrace();
+		}//catch block	
+	}//file reader method
+	
+	private static void createDfa() {
+		for(int i = 0;i<alph.size();i++) {
+			outer.put(alph.get(i), createTransition(alph.get(i)));
 		}
 		
 	}
-	
-	private HashMap<E, HashMap> createDfa(HashMap dfa, ArrayList alph, ArrayList states){
-		return dfa;
+
+	private static HashMap<Character, Character> createTransition(char a) {
+		inner = new HashMap<Character, Character>();
+		
+		while(a == transitions.peekFirst()) {
+			transitions.removeFirst();
+			inner.put(transitions.poll(), transitions.poll());
+		}
+		return inner;
 	}
+	
+	private void improperString() {
+		System.out.println("The string you entered is not a proper language for this DFA");
+	}
+
+	
 }
