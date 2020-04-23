c90e31f946896a25fb8af58675cfd698a2adb7b0
bl03360 <54645762+bl03360@users.noreply.github.com>
2020-04-22 20:31:11 -0400

    Add files via upload

--- DFA.java
+++ DFA.java
@@ -3,7 +3,6 @@ package DefiniteFiniteAutomata;
 import java.io.*;
 import java.util.*;
 
-
 public class DFA {
 
 	private static HashMap<Character, Character> inner;
@@ -13,91 +12,127 @@ public class DFA {
 	private static ArrayList<Character> finalStates = new ArrayList<>();
 	private static LinkedList<Character> transitions = new LinkedList<>();
 	private static char startState;
-	
-	
+
 	public static void main(String[] args) {
-	
+
 		char marker = 0;
-		
+
 		fileReader();
+		
+		for(int i = 0;i<alph.size();i++) {
+			System.out.println("This is the alph at " + i + " " + alph.get(i));
+		}
+		for(int i = 0;i<states.size();i++) {
+			System.out.println("This is the state at " + i + " " + states.get(i));
+		}
+		for(int i = 0;i<finalStates.size();i++) {
+			System.out.println("This is the final state at " + i + " " +finalStates.get(i));
+		}
+		for(int i = 0;i<transitions.size();i++) {
+			System.out.print(transitions.get(i));
+		}
+		System.out.println();
+		
 		createDfa();
 		
+		for (int i = 0; i < states.size(); i++) {
+			System.out.println("At state " + states.get(i) + " " + outer.get(states.get(i)));
+		}
+
 		Scanner scan = new Scanner(System.in);
 		
+		System.out.print("Enter a string of the alphabet, " + alph.toString() + ", to generate a proper string of the dfa.");
+		String userInput = scan.next();
+		
+		traverseDFA(userInput);
 
+	}
+
+	private static void traverseDFA(String userInput) {
+		ArrayList<Character> lang = new ArrayList<>();
+		
+		lang.add(startState);
+		
 	}
 
 	private static void fileReader() {
 		try {
-			File file = new File("");
+			File file = new File("DFA.txt");
 			FileReader fr = new FileReader(file);
-			BufferedReader br = new  BufferedReader(fr);
+			BufferedReader br = new BufferedReader(fr);
 			String line = br.readLine();
+			line = line.replaceAll("[^a-zA-Z0-9]", "");
 			
 			int count = 0;
-			
+
 			while (line != null) {
 				
-				line.replaceAll("[^a-zA-Z0-9]+", "");
-				StringBuilder sb = new StringBuilder();
-				sb.append(line);
-				
-				while(count != 3) {
-					if(count == 0) {
-						for(int i = 0;i<sb.length();i++) {
-							alph.add(sb.charAt(i));
+				while (count <= 3) {
+					
+					if (count == 0) {
+						for (int i = 0; i < line.length(); i++) {
+							alph.add(line.charAt(i));
+							System.out.println(line + " " + count);
 						}
-					}//reads alphabet from line 1 from file
-					else if(count == 1) {
-						for(int i = 0;i<sb.length();i++) {
-							states.add(sb.charAt(i));
+					} // reads alphabet from line 1 from file
+					else if (count == 1) {
+						for (int i = 0; i < line.length(); i++) {
+							states.add(line.charAt(i));
+							System.out.println(line + " " + count);
 						}
-					}//reads states from line 2 from file
-					else if(count ==  2){
-						startState = sb.charAt(0);
-					}//reads start state from line 3 from file
+					} // reads states from line 2 from file
+					else if (count == 2) {
+						startState = line.charAt(0);
+						System.out.println(line + " " + count);
+					} // reads start state from line 3 from file
 					else {
-						for(int i = 0;i<sb.length();i++) {
-							finalStates.add(sb.charAt(i));
+						for (int i = 0; i < line.length(); i++) {
+							finalStates.add(line.charAt(i));
+							System.out.println(line + " " + count);
 						}
-					}//reads final states
-					sb.append("");
+					} // reads final states
 					line = br.readLine();
+					line = line.replaceAll("[^a-zA-Z0-9]", "");
 					count++;
-				}//reads first 4 lines for the alphabet, states, start state, and final states
+				} // reads first 4 lines for the alphabet, states, start state, and final states
 				
-				for(int i = 0;i<sb.length();i++) {
-						transitions.add(sb.charAt(i));
-				}//reads transitions from file adding from rest of the lines
+				line = line.replaceAll("[^a-zA-Z0-9]", "");
 				
-			}//while loop to read all lines of file
-		
-		}//try block
-		catch(IOException error) {
+				for (int i = 0; i < line.length(); i++) {
+					transitions.add(line.charAt(i));
+					System.out.println(line + " " + count);
+				} // reads transitions from file adding from rest of the lines
+				
+				line = br.readLine();
+				
+			} // while loop to read all lines of file
+
+		} // try block
+		catch (IOException error) {
 			error.printStackTrace();
-		}//catch block	
-	}//file reader method
-	
+		} // catch block
+	}// file reader method
+
 	private static void createDfa() {
-		for(int i = 0;i<alph.size();i++) {
-			outer.put(alph.get(i), createTransition(alph.get(i)));
+		for (int i = 0; i < states.size(); i++) {
+			System.out.println(" ok new problem" + " " + states.get(i));
+			outer.put(states.get(i), createTransition(states.get(i)));
 		}
-		
 	}
 
 	private static HashMap<Character, Character> createTransition(char a) {
 		inner = new HashMap<Character, Character>();
 		
-		while(a == transitions.peekFirst()) {
+		while (transitions.peek() != null && a == transitions.peekFirst()) {
 			transitions.removeFirst();
 			inner.put(transitions.poll(), transitions.poll());
 		}
+		System.out.println(inner.toString());
 		return inner;
 	}
-	
+
 	private void improperString() {
 		System.out.println("The string you entered is not a proper language for this DFA");
 	}
 
-	
 }
